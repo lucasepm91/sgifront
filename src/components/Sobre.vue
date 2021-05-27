7834e5e637dc902aee07f9697eaaa9dc3a720d03
Lucas Martins <lucas.martins@ed.company>
2021-05-26 21:26:27 -0300

    alterando store de eventos e melhorando componente sobre

--- src/components/Sobre.vue
+++ src/components/Sobre.vue
@@ -1,5 +1,17 @@
 <template>
-  <h2>Sobre</h2>
+<div>
+  <div id="divTituloSobre">
+      <p>Sobre</p>      
+  </div>
+
+  <div id="descricaoSobre">
+    <p>A SGI tem como missão facilitar a compra de ingressos para eventos, reunindos os melhores espetáculos para a sua diversão</p>
+  </div>
+
+  <div id="imagemSobre">
+    <b-img :src="require('../assets/concerto.png')"  fluid alt="imagem de um show"></b-img>
+  </div>
+</div>
 </template>
 
 <script>
@@ -8,6 +20,29 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+#divTituloSobre{
+    margin-top: 50px;
+    text-align: center;
+}
+#divTituloSobre p{
+    color: dimgray;
+    font-size: 1.5em;
+    font-style: italic;
+}
+#descricaoSobre{
+    margin-top: 50px;
+    text-align: center;
+    margin-bottom: 30px;
+}
+#descricaoSobre p{
+    color: dimgray;
+    font-size: 1.5em;
+    font-style: italic;
+}
+#imagemSobre{
+  width: 100%;
+  text-align: center;
+  margin-bottom: 50px;
+}
 </style>
--- src/store/modules/evento.js
+++ src/store/modules/evento.js
@@ -1,28 +1,126 @@
 import axios from "axios";
 
 const state = {
-    eventos: []
+    shows: [],
+    filmes: [],
+    pecas: [],
+    historico: []
 };
 
 const getters = {
-    allEventos: state => state.eventos
+    todosShows: state => state.shows,
+    todosFilmes: state => state.filmes,
+    todosPecas: state => state.pecas,
+    todosHistorico: state => state.historico
 };
 
 const actions = {
-    getEventos({ commit }) {
+    buscarShows({ commit }) {
         axios.get(
-            "http://localhost:8081/pbsgi/eventos"
+            "http://localhost:8080/pbsgi/evento/shows"
         ).then((response) => {
-            commit('getEventos', response.data);
+            commit('buscarShows', response.data);
         });
+    },
 
-    }
+    buscarFilmes({ commit }) {
+        axios.get(
+            "http://localhost:8080/pbsgi/evento/filmes"
+        ).then((response) => {
+            commit('buscarFilmes', response.data);
+        });
+    },
+
+    buscarPecasTeatro({ commit }) {
+        axios.get(
+            "http://localhost:8080/pbsgi/evento/pecas"
+        ).then((response) => {
+            commit('buscarPecasTeatro', response.data);
+        });
+    },
+
+    buscarHistorico({ commit }, usuarioId) {
+        axios.get(
+            "http://localhost:8080/pbsgi/evento/historico/"+usuarioId
+        ).then((response) => {
+            commit('buscarHistorico', response.data);
+        });
+    },
+
+    buscarEvento({ commit }, eventoId) {
+        axios.get(
+            "http://localhost:8080/pbsgi/evento/historico/"+eventoId
+        ).then((response) => {
+            commit('buscarEvento', response.data);
+        });
+    },
+
+    criarEvento({ commit }, evento){ 
+        const headers = { 
+          "Content-Type": "application/json"
+        };
+    
+        axios.post(
+          "http://localhost:8080/evento", evento, {headers}
+        ).then((response) => {      
+          commit('criarEvento', response.data);
+        });
+        
+    },
+
+    deletarEvento({ commit }, id){
+        const headers = { 
+          "Content-Type": "application/json"
+        };
+    
+        axios.delete(
+          "http://localhost:8080/evento/"+id, {headers}
+        ).then((response) => {      
+          commit('deletarEvento', response.data.id);
+        });
+        
+    },
+    
+    atualizarEvento({ commit }, atlzEvento) {  
+        const headers = { 
+          "Content-Type": "application/json"
+        };
+    
+        axios.put(
+          "http://localhost:8080/evento/"+atlzEvento.id, atlzEvento, {headers}
+        ).then((response) => {      
+          commit("atualizarEvento", response.data);
+        });
+    }      
+      
 }
 
 const mutations = {
-    getEventos: (state, data) =>{
-        state.eventos = data;
+    buscarShows: (state, data) =>{
+        state.shows = data;
+    },
+    buscarFilmes: (state, data) =>{
+        state.filmes = data;
+    },
+    buscarPecasTeatro: (state, data) =>{
+        state.pecas = data;
+    },
+    buscarHistorico: (state, data) =>{
+        state.pecas = data;
+    },
+    criarEvento: (state, data) =>{
+        state.historico.push(data);
+    },
+    deletarEvento: (state, id) =>{
+        state.historico = state.historico.filter(e => e.id != id);
+    },
+    atualizarEvento: (state, data) => {
+        const index = state.historico.findIndex(e => e.id === data.id)
+        if (index !== -1) {        
+          state.historico.splice(index, 1, data);
+        }
     }
+
 }
 
 export default {
